443042012a8574e5cc466e4003f7c318588c1c3f
Nikita <nothanks000@gmail.com>
2016-10-12 12:37:27 +0300

    some changes, finished console game

--- src/ru/geerbrains/guess_the_number/Main.java
+++ src/ru/geerbrains/guess_the_number/Main.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Ваша задача угадать число.");
-        int range = 30;
+        for(int i = 10; i <= 30; i += 10) playLevel(i);
+        System.out.println("Вы выиграли!!!");
+        scanner.close();
+    }
+    private static void playLevel(int range){
         int number = (int)(Math.random() * range);
         while (true) {
             System.out.println("Угадайте число от 0 до " + range);
@@ -21,6 +26,5 @@ public class Main {
                 System.out.println("Загаданное число больше");
             }
         }
-        scanner.close();
     }
 }
